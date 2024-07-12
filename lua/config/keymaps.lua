0efe6a401b66abe042ecdfc72a97c7b1e41f282a
keypeearr <kyle.requez.code@gmail.com>
2024-07-13 03:11:28 +0800

    Added New Keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,11 @@ local opts = { noremap = true, silent = true }
 -- General
 -- Removes any commands and goes to normal mode
 vim.api.nvim_set_keymap('n', '<Esc>', '<Esc>:nohl<CR>', opts)
+-- Resizes Current Buffer
+vim.api.nvim_set_keymap('n', '<leader><left>', ':buffer vertical resize -5<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader><right>', ':buffer vertical resize +5<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader><up>', ':buffer resize -5<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader><down>', ':buffer resize +5<CR>', opts)
 
 -- BarBar
 -- Move to previous/next
@@ -16,4 +21,9 @@ vim.api.nvim_set_keymap('n', '<leader>x', '<Cmd>BufferClose<CR>', opts)
 -- Restore previously closed buffer
 vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>BufferRestore<CR>', opts)
 
+-- Toggle Term
+-- Toggle terminal at the bottom half of the screen
+vim.api.nvim_set_keymap('n', '<leader>t', ':ToggleTerm direction=horizontal<CR>', opts)
 
+-- Toggle terminal to the side
+vim.api.nvim_set_keymap('n', '<leader>T', ':ToggleTerm direction=vertical<CR>', opts)
