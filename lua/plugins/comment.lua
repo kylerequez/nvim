21035e2fa5d0f7b1e4b88dd86f64caca07196d93
keypeearr <kyle.requez.code@gmail.com>
2024-08-27 10:11:20 +0800

    Changed keymaps for comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,9 +2,9 @@ return {
   'EvgeniGenchev/comment-nvim',
   config = function()
     -- Change the default singleline comment
-    vim.api.nvim_set_keymap('n', '<leader>/', ':Comment<CR>', { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('n', 'gcc', ':Comment<CR>', { noremap = true, silent = false })
 
     -- Change the visual mode multiline comment
-    vim.api.nvim_set_keymap('v', '<leader>/', ':CommentMore<CR>', { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('v', 'gcc', ':CommentMore<CR>', { noremap = true, silent = false })
   end,
 }
