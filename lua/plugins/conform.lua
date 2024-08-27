9c3d7f25bf7b76bba1b09c8474d187e9f1701a80
keypeearr <kyle.requez.code@gmail.com>
2024-08-28 01:35:39 +0800

    Added colorizer

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,15 +1,15 @@
 return { -- Autoformat
 	'stevearc/conform.nvim',
-	event = { 'BufWritePre' },
+	event = { 'BufWritePre', 'BufNewFile' },
 	cmd = { 'ConformInfo' },
 	keys = {
 		{
-			'<leader>w',
+			'<leader>F',
 			function()
 				require('conform').format { async = true, lsp_format = 'fallback' }
 			end,
 			mode = '',
-			desc = 'Format and [W]rite buffer',
+			desc = '[F]ormat buffer',
 		},
 	},
 	opts = {
@@ -32,43 +32,14 @@ return { -- Autoformat
 		end,
 		formatters_by_ft = {
 			lua = { 'stylua' },
+			go = { 'gofumpt', 'goimports', 'goimportsreviser', 'golines', 'gomodifytags' },
+			templ = { 'templ', 'tailwindcss-language-server', 'prettierd', 'prettier' },
+			sql = { 'sql-formatter', 'sqlfluff', 'sqlls' }
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
-	},
-	{
-		"debugloop/telescope-undo.nvim",
-		dependencies = { -- note how they're inverted to above example
-			{
-				"nvim-telescope/telescope.nvim",
-				dependencies = { "nvim-lua/plenary.nvim" },
-			},
-		},
-		keys = {
-			{ -- lazy style key map
-				"<leader>u",
-				"<cmd>Telescope undo<cr>",
-				desc = "undo history",
-			},
-		},
-		opts = {
-			-- don't use `defaults = { }` here, do this in the main telescope spec
-			extensions = {
-				undo = {
-					-- telescope-undo.nvim config, see below
-				},
-				-- no other extensions here, they can have their own spec too
-			},
-		},
-		config = function(_, opts)
-			-- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-			-- configs for us. We won't use data, as everything is in it's own namespace (telescope
-			-- defaults, as well as each extension).
-			require("telescope").setup(opts)
-			require("telescope").load_extension("undo")
-		end,
-	},
+	}
 }
