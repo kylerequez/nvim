95a9444ece6984f01fbc7caf9843d70e738eff1d
kylerequez <kyle.requez.code@gmail.com>
2024-10-14 08:50:18 +0800

    Added keybinds for split navigation

--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -1,12 +1,13 @@
+local set = vim.keymap.set
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+set("n", "<leader>Q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Deletes current buffer without saving
-vim.keymap.set('n', '<leader>q', ':bdelete!<CR>', { desc = "[Q]uit Current Buffer", noremap = true, silent = true })
+set("n", "<leader>q", ":bdelete!<CR>", { desc = "[Q]uit Current Buffer", noremap = true, silent = true })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -14,8 +15,13 @@ vim.keymap.set('n', '<leader>q', ':bdelete!<CR>', { desc = "[Q]uit Current Buffe
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Create a keymap to create a new terminal buffer
-vim.keymap.set("n", "<leader>T", ":terminal<CR>", {desc="Create [T]erminal", noremap = true, silent = true })
+set("n", "<leader>T", ":terminal<CR>", { desc = "Create [T]erminal", noremap = true, silent = true })
 
+-- Allows easier transition from splits
+set("n", "<c-j>", "<c-w><c-j>")
+set("n", "<c-k>", "<c-w><c-k>")
+set("n", "<c-h>", "<c-w><c-h>")
+set("n", "<c-l>", "<c-w><c-l>")
