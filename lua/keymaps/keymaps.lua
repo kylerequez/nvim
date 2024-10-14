05aedf581d85f9f52bddc991709baadd08f30ba6
kylerequez <kyle.requez.code@gmail.com>
2024-10-14 09:37:58 +0800

    Added keymaps for split creation and resizing

--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -20,8 +20,18 @@ set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 -- Create a keymap to create a new terminal buffer
 set("n", "<leader>T", ":terminal<CR>", { desc = "Create [T]erminal", noremap = true, silent = true })
 
+-- Allows creation of splits
+set("n", "<leader>H", ":split<CR>", { desc = "Create Horizontal Split", noremap = true, silent = true })
+set("n", "<leader>V", ":vsplit<CR>", { desc = "Create Vertical Split", noremap = true, silent = true })
+
 -- Allows easier transition from splits
-set("n", "<c-j>", "<c-w><c-j>")
-set("n", "<c-k>", "<c-w><c-k>")
-set("n", "<c-h>", "<c-w><c-h>")
-set("n", "<c-l>", "<c-w><c-l>")
+set("n", "<c-j>", "<c-w><c-j>", { desc = "Split Move Down", noremap = true, silent = true })
+set("n", "<c-k>", "<c-w><c-k>", { desc = "Split Move Up", noremap = true, silent = true })
+set("n", "<c-h>", "<c-w><c-h>", { desc = "Split Move Left", noremap = true, silent = true })
+set("n", "<c-l>", "<c-w><c-l>", { desc = "Split Move Right", noremap = true, silent = true })
+
+-- Allows resizing of splits
+set("n", "<leader>rh", "<c-w>5<", { desc = "Split [R]esize Left", noremap = true, silent = true })
+set("n", "<leader>rl", "<c-w>5>", { desc = "Split [R]esize Right", noremap = true, silent = true })
+set("n", "<leader>rk", "<c-w>+", { desc = "Split [R]esize Up", noremap = true, silent = true })
+set("n", "<leader>rj", "<c-w>-", { desc = "Split [R]esize Down", noremap = true, silent = true })
