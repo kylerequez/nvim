69dde09aa242700b5b48648b463e99f3b6cf4fd7
kylerequez <kyle.requez.code@gmail.com>
2025-01-02 09:07:16 +0800

    Added markview

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+return {
+	"OXY2DEV/markview.nvim",
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		filetypes = { "markdown" },
+	},
+}
