36415491481e964686d825e5ade15b61df8bd0b1
kylerequez <kyle.requez.code@gmail.com>
2025-01-16 02:31:33 +0800

    Removed carbon now

--- lua/plugins/carbon-now.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"ellisonleao/carbon-now.nvim",
-	lazy = true,
-	cmd = "CarbonNow",
-	opts = {},
-	keys = {
-		{ mode = "v", "<leader>cn", ":CarbonNow<CR>", desc = "[C]arbon [N]ow" },
-	},
-}
