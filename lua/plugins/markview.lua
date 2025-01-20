47bd344257401328abd1b88bd766d22e44c33099
kpr <kyle.requez.code@gmail.com>
2025-01-20 23:06:42 +0800

    Added keybindings for markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -9,4 +9,9 @@ return {
 	opts = {
 		filetypes = { "markdown" },
 	},
+	keys = {
+		{ mode = "n", "<leader>mt", "<CMD>Markview toggleAll<CR>", desc = "[M]arkview [T]oggle" },
+		{ mode = "n", "<leader>ms", "<CMD>Markview splitToggle<CR>", desc = "[M]arkview [S]plit Toggle" },
+		{ mode = "n", "<leader>mo", "<CMD>MarkOpen<CR>", desc = "[M]arkview [O]pen" },
+	},
 }
