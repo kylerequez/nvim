3c9e116656980b2d12664af9618d29812c2fc8f6
kylerequez <kyle.requez.code@gmail.com>
2025-04-12 12:53:45 +0800

    Updated snacks dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -17,9 +17,7 @@ return {
 				},
 				sections = {
 					{ section = "header" },
-					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{ section = "keys", gap = 1, padding = 1 },
 					{ section = "startup" },
 				},
 			},
