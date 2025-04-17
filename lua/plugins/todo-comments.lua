782dec5a3645ccaf2609a5937aaf756c13709aa4
kylerequez <kyle.requez.code@gmail.com>
2025-04-17 22:30:27 +0800

    Added todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,5 +2,10 @@ return {
 	"folke/todo-comments.nvim",
 	event = "BufRead",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = { signs = false },
+	opts = {
+		signs = false,
+	},
+	keys = {
+		{ "<leader>fo", "<cmd>TodoFzfLua<CR>", desc = "[F]ind t[O]do" },
+	},
 }
