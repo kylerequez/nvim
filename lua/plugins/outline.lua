299de0368753b524d05f21e0bc07f1f32a951b3d
keypeearr <kyle.requez.code@gmail.com>
2025-06-21 23:24:15 +0800

    Updated symbols-outline to outline

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,8 +1,9 @@
 return {
 	"hedyhli/outline.nvim",
+	lazy = true,
 	cmd = { "Outline", "OutlineOpen" },
-	opts = {},
 	keys = {
 		{ "<leader>ss", "<cmd>Outline<CR>", desc = "[S]how [S]ymbols" },
 	},
+	opts = {},
 }
